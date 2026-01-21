af85ada17364b912fd0fa285fc4c5a815ba7d02d
headspur-projects <tanish@headspur.it>
2026-01-21 14:36:15 +0530

    edit account user account page changes

--- masterstudy-lms-learning-management-system/_core/stm-lms-templates/account/private/parts/edit_account.php
+++ masterstudy-lms-learning-management-system/_core/stm-lms-templates/account/private/parts/edit_account.php
@@ -5,56 +5,54 @@
 
 $is_instructor = STM_LMS_Instructor::is_instructor();
 
-wp_enqueue_style( 'masterstudy-select2' );
-wp_enqueue_script( 'masterstudy-personal-info' );
 stm_lms_register_style( 'edit_account' );
-stm_lms_register_script( 'edit_account' );
+stm_lms_register_script( 'edit_account', array( 'vue.js', 'vue-resource.js' ) );
 stm_lms_register_style( 'user_info_top' );
 
 if ( ! metadata_exists( 'user', $current_user['id'], 'disable_report_email_notifications' ) ) {
 	$current_user['meta']['disable_report_email_notifications'] = true;
 }
 
-$data              = wp_json_encode( $current_user );
-$personal_data_raw = get_user_meta( $current_user['id'], 'masterstudy_personal_data', true );
-
-if ( ! is_array( $personal_data_raw ) ) {
-	$personal_data_raw = array();
-}
-
-$personal_data = wp_json_encode( (object) $personal_data_raw );
-
+$data = wp_json_encode( $current_user );
 wp_add_inline_script(
 	'stm-lms-edit_account',
-	"var stm_lms_edit_account_info = {$data};
-	var stm_lms_personal_data = {$personal_data};"
+	"var stm_lms_edit_account_info = {$data}"
 );
+
 ?>
 
 
 <div class="stm_lms_edit_account" id="stm_lms_edit_account">
+	<div class="stm_lms_edit_account_innerr">
+		<?php
+		// Add top_info section inside edit_account
+		STM_LMS_Templates::show_lms_template(
+			'account/private/parts/top_info',
+			array( 'current_user' => $current_user )
+		);
+		?>
 
-	<div class="stm_lms_edit_socials stm_lms_edit_name">
-		<div class="stm_lms_edit_socials_list">
-			<?php
-			if ( $is_instructor ) {
-				STM_LMS_Templates::show_lms_template( 'account/private/edit_account/profile-cover' );
-			}
-			?>
-			<?php STM_LMS_Templates::show_lms_template( 'account/private/edit_account/name' ); ?>
+		<div class="stm_lms_edit_socials stm_lms_edit_name">
+			<div class="stm_lms_edit_socials_list">
+				<?php
+				if ( $is_instructor ) {
+					STM_LMS_Templates::show_lms_template( 'account/private/edit_account/profile-cover' );
+				}
 
-			<?php
-			if ( $is_instructor ) {
-				STM_LMS_Templates::show_lms_template( 'account/private/edit_account/position' );
-				STM_LMS_Templates::show_lms_template( 'account/private/edit_account/bio' );
-			}
+				STM_LMS_Templates::show_lms_template( 'account/private/edit_account/name' );
 
-			STM_LMS_Templates::show_lms_template( 'account/private/edit_account/display_name' );
-			?>
+				if ( $is_instructor ) {
+					STM_LMS_Templates::show_lms_template( 'account/private/edit_account/position' );
+					STM_LMS_Templates::show_lms_template( 'account/private/edit_account/bio' );
+				}
 
-			<?php STM_LMS_Templates::show_lms_template( 'account/private/edit_account/custom_fields' ); ?>
+				STM_LMS_Templates::show_lms_template( 'account/private/edit_account/display_name' );
+				STM_LMS_Templates::show_lms_template( 'account/private/edit_account/custom_fields' );
+				?>
+			</div>
 		</div>
 	</div>
+
 	<?php
 	$email_settings    = get_option( 'stm_lms_email_manager_settings' );
 	$student_digest    = $email_settings['stm_lms_reports_student_checked_enable'] ?? false;
@@ -86,23 +84,37 @@ wp_add_inline_script(
 	?>
 
 	<div class="row">
+
 		<div class="col-md-12">
+
 			<div class="row">
+
 				<div class="col-md-6 col-sm-6">
-					<button class="btn btn-default btn-save-account">
+
+					<button @click="saveUserInfo()"
+							v-bind:class="{'loading' : loading}"
+							class="btn btn-default btn-save-account">
 						<span><?php esc_html_e( 'Save changes', 'masterstudy-lms-learning-management-system' ); ?></span>
 					</button>
+
 				</div>
+
 				<div class="col-md-6 col-sm-6">
+
 					<div class="stm_lms_sidebar_logout_wrapper text-right xs-text-left">
 						<?php STM_LMS_Templates::show_lms_template( 'account/private/parts/logout' ); ?>
 					</div>
+
 				</div>
+
 			</div>
+
 		</div>
+
 		<div class="col-md-12">
 			<div class="stm-lms-message masterstudy-edit-account-message-hidden">
 			</div>
 		</div>
 	</div>
+
 </div>
